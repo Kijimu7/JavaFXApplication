c14ec636679f104384b265a0162362c8bfb019ab
Kijimj7 <pinkokinawa84@gmail.com>
2020-06-14 15:45:19 -0700

    Initial commit

---
+++ src/javafxapplication/MainscreenController.java
@@ -0,0 +1,147 @@
+
+./*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javafxapplication;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;./*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javafxapplication;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
+/**
+ * FXML Controller class
+ *
+ * @author pinko
+ */
+public class MainscreenController implements Initializable {
+
+    @FXML
+    private Button Search;
+    @FXML
+    private TextField SearchText;
+    @FXML
+    private TableView<?> table;
+    @FXML
+    private TableColumn<?, ?> Id;
+    @FXML
+    private TableColumn<?, ?> Name;
+    @FXML
+    private TableColumn<?, ?> inStock;
+    @FXML
+    private TableColumn<?, ?> Price;
+    @FXML
+    private Button Search1;
+    @FXML
+    private TextField SearchText1;
+    @FXML
+    private TableView<?> table1;
+    @FXML
+    private TableColumn<?, ?> Id1;
+    @FXML
+    private TableColumn<?, ?> Name1;
+    @FXML
+    private TableColumn<?, ?> inStock1;
+    @FXML
+    private TableColumn<?, ?> Price1;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void searchHandler(ActionEvent event) {
+    }
+
+    @FXML
+    private void addHandler(ActionEvent event) {
+    }
+
+    @FXML
+    private void modifyHandler(ActionEvent event) {
+    }
+
+    @FXML
+    private void deleteHandler(ActionEvent event) {
+    }
+
+    @FXML
+    private void addHandler1(ActionEvent event) {
+    }
+
+    @FXML
+    private void modifyHandler1(ActionEvent event) {
+    }
+
+    @FXML
+    private void deleteHandler1(ActionEvent event) {
+    }
+
+    @FXML
+    private void exitHandler(ActionEvent event) {
+    }
+
+    @FXML
+    private void searchHandler(MouseEvent event) {
+    }
+
+    @FXML
+    private void addHandler(MouseEvent event) {
+    }
+
+    @FXML
+    private void modifyHandler(MouseEvent event) {
+    }
+
+    @FXML
+    private void deleteHandler(MouseEvent event) {
+    }
+
+    @FXML
+    private void Search1(MouseEvent event) {
+    }
+
+    @FXML
+    private void addHandler1(MouseEvent event) {
+    }
+
+    @FXML
+    private void modifyHandler1(MouseEvent event) {
+    }
+
+    @FXML
+    private void deleteHandler1(MouseEvent event) {
+    }
+
+    @FXML
+    private void exitHandler(MouseEvent event) {
+    }
+    
+}
---
+++ src/javafxapplication/Part.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javafxapplication;
+
+/**
+ *
+ * @author pinko
+ */
+public abstract class Part {
+
+   //Declare field
+   private int id;
+   private String name;
+   private double price;
+   private int stock;
+   private int min;
+   private int max;
+   
+}
