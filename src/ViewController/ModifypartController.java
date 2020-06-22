66cad382ed5a2299b4f5612353b122d0c093820f
Kijimj7 <pinkokinawa84@gmail.com>
2020-06-22 10:33:38 -0700

    Added all controller files

---
+++ src/ViewController/ModifypartController.java
@@ -0,0 +1,15 @@
+package ViewController;
+
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ModifypartController implements Initializable {
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+}
