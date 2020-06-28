39f3fed79dab7fc108e0614f4462fafdff1f9675
Kijimj7 <pinkokinawa84@gmail.com>
2020-06-28 00:57:37 -0700

    Save button is transferring the data to the main system window but it is not loading the data correctly.

--- src/Main/InventoryProgram.java
+++ src/Main/InventoryProgram.java
@@ -52,28 +52,24 @@ InventoryProgram extends Application {
         InHouse part1 = new InHouse(1, "part1", 5.00, 10, 1, 100, 101);
         InHouse part2 = new InHouse(2, "part2", 6.00, 20, 1, 100, 102);
         InHouse part3 = new InHouse(3, "part3", 7.00, 30, 1, 100, 103);
-        InHouse part4 = new InHouse(4, "part4", 8.00, 40, 1, 100, 104);
-        InHouse part5 = new InHouse(5, "part5", 9.00, 50, 1, 100, 105);
+
 
         Inventory.addPart(part1);
         Inventory.addPart(part2);
         Inventory.addPart(part3);
-        Inventory.addPart(part4);
-        Inventory.addPart(part5);
+
 
         //Outsourced parts
         Outsourced part6 = new Outsourced(6, "part6", 10.00, 60, 1, 100, "company1");
-        Outsourced part7 = new Outsourced(7, "part7", 11.00, 70, 1, 100, "company1");
-        Outsourced part8 = new Outsourced(8, "part8", 12.00, 80, 1, 100, "company1");
-        Outsourced part9 = new Outsourced(9, "part9", 13.00, 90, 1, 100, "company1");
-        Outsourced part10 = new Outsourced(10, "part10", 14.00, 100, 1, 100, "company1");
+        Outsourced part7 = new Outsourced(7, "part7", 11.00, 70, 1, 100, "company2");
+        Outsourced part8 = new Outsourced(8, "part8", 12.00, 80, 1, 100, "company3");
+
 
 
         Inventory.addPart(part6);
         Inventory.addPart(part7);
         Inventory.addPart(part8);
-        Inventory.addPart(part9);
-        Inventory.addPart(part10);
+
 
         //Products
 
--- src/ViewController/AddpartController.java
+++ src/ViewController/AddpartController.java
@@ -1,18 +1,17 @@
 package ViewController;
 
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.event.ActionEvent;
 import javafx.scene.control.*;
 import javafx.stage.Stage;
 import model.InHouse;
 import model.Inventory;
 import model.Outsourced;
-import sun.plugin.dom.html.HTMLBodyElement;
 
 import java.io.IOException;
 import java.net.URL;
@@ -26,8 +25,10 @@ public class AddpartController implements Initializable {
     Stage stage;
     Parent scene;
     private Object ResourceBundle;
-    private HTMLBodyElement addPartDynamicTxt;
+    @FXML
+
     private Object ActionEvent;
+
 
     @Override
     public void initialize(URL location, java.util.ResourceBundle resources) {
@@ -38,7 +39,9 @@ public class AddpartController implements Initializable {
 
     }
 
-    public AddpartController(Inventory inventory) { this.inventory = inventory; }
+    public AddpartController(Inventory inventory) {
+        this.inventory = inventory;
+    }
 
 
     @FXML
@@ -78,6 +81,9 @@ public class AddpartController implements Initializable {
     private TextField addPartIdTxt;
 
     @FXML
+    private TextField addPartDynamicTxt;
+
+    @FXML
     private Button addPartsaveBtn;
 
     @FXML
@@ -85,63 +91,93 @@ public class AddpartController implements Initializable {
 
 
     @FXML
-    void inhouseRadioButtonSelected(javafx.event.ActionEvent event){
+    void inhouseRadioButtonSelected(javafx.event.ActionEvent event) {
         isOutsourced = false;
         addPartCnameLbl.setText("Machine ID");
     }
+
     @FXML
-    void outsourcedRadioButtonSelected(javafx.event.ActionEvent event){
+    void outsourcedRadioButtonSelected(javafx.event.ActionEvent event) {
         isOutsourced = true;
         addPartCnameLbl.setText("Company Name");
     }
 
-    public void onActionCancel (javafx.event.ActionEvent event) throws IOException {
 
-        stage = (Stage) ((Button) event.getSource()).getScene().getWindow();
-        scene = FXMLLoader.load(getClass().getResource("MainScreen.fxml"));
-        stage.setScene(new Scene((Parent) scene));
-        stage.show();
-    }
-
-
-
+    @FXML
     void onActionSavePart(ActionEvent event) throws IOException {
 
 
+        //Create auto Ids
+        Random random = new Random();
+        int id = random.nextInt(50);
 
-            //Create auto Ids
-            Random random = new Random();
-            int id = random.nextInt(50);
 
+        if (isOutsourced == false) {
 
-            if (isOutsourced == false) {
+            InHouse newInHouse = new InHouse(0, "", 0, 0, 0, 0, 0);
 
-                InHouse newInHouse = new InHouse(0, "", 0, 0, 0, 0, 0);
+            if (isValid(addPartNameTxt.getText(), addPartPriceTxt.getText(), addPartInvTxt.getText(), addPartMinTxt.getText(),
+                    addPartMaxTxt.getText(), addPartDynamicTxt.getText())) {
 
-                if (isValid(addPartNameTxt.getText(), addPartPriceTxt.getText(), addPartInvTxt.getText(), addPartMinTxt.getText(),
-                        addPartMaxTxt.getText(), addPartDynamicTxt.getText())) {
+                newInHouse.setId(id);
+                if (!addPartNameTxt.getText().isEmpty()) {
+                    newInHouse.setName(addPartNameTxt.getText());
+                }
+                if (!addPartPriceTxt.getText().isEmpty()) {
+                    newInHouse.setName(addPartPriceTxt.getText());
+                }
+                if (!addPartInvTxt.getText().isEmpty()) {
+                    newInHouse.setName(addPartInvTxt.getText());
+                }
+                if (!addPartMinTxt.getText().isEmpty()) {
+                    newInHouse.setName(addPartMinTxt.getText());
+                }
+                if (!addPartMaxTxt.getText().isEmpty()) {
+                    newInHouse.setName(addPartMaxTxt.getText());
+                }
+                if (!addPartDynamicTxt.getText().isEmpty()) {
+                    newInHouse.setName(addPartDynamicTxt.getText());
+                }
 
-                    newInHouse.setId(id);
+                inventory.addPart(newInHouse);
+                System.out.println("Part Added");
+
+                Stage stage;
+                Parent root;
+                stage = (Stage) addPartsaveBtn.getScene().getWindow();
+                FXMLLoader loader = new FXMLLoader(getClass().getResource("/ViewController/MainScreen.fxml"));
+                root = loader.load();
+                Scene scene = new Scene(root);
+                stage.setScene(scene);
+                stage.show();
+            }
+
+            }else {
+                Outsourced newOutsourced = new Outsourced(0, "", 0, 0, 0, 0, "");
+
+                if (isValid(addPartNameTxt.getText(), addPartPriceTxt.getText(), addPartInvTxt.getText(), addPartMinTxt.getText(), addPartMaxTxt.getText(), addPartDynamicTxt.getText())) {
+
+                    newOutsourced.setId(id);
                     if (!addPartNameTxt.getText().isEmpty()) {
-                        newInHouse.setName(addPartNameTxt.getText());
+                        newOutsourced.setName(addPartNameTxt.getText());
                     }
                     if (!addPartPriceTxt.getText().isEmpty()) {
-                        newInHouse.setName(addPartPriceTxt.getText());
+                        newOutsourced.setName(addPartPriceTxt.getText());
                     }
                     if (!addPartInvTxt.getText().isEmpty()) {
-                        newInHouse.setName(addPartInvTxt.getText());
+                        newOutsourced.setName(addPartInvTxt.getText());
                     }
                     if (!addPartMinTxt.getText().isEmpty()) {
-                        newInHouse.setName(addPartMinTxt.getText());
+                        newOutsourced.setName(addPartMinTxt.getText());
                     }
                     if (!addPartMaxTxt.getText().isEmpty()) {
-                        newInHouse.setName(addPartMaxTxt.getText());
+                        newOutsourced.setName(addPartMaxTxt.getText());
                     }
                     if (!addPartDynamicTxt.getText().isEmpty()) {
-                        newInHouse.setName(addPartDynamicTxt.getText());
+                        newOutsourced.setName(addPartDynamicTxt.getText());
                     }
 
-                    inventory.addPart(newInHouse);
+                    inventory.addPart(newOutsourced);
                     System.out.println("Part Added");
 
                     Stage stage;
@@ -154,96 +190,142 @@ public class AddpartController implements Initializable {
                     stage.show();
 
 
-                }else{
-                    Outsourced newOutsourced = new Outsourced(0, "", 0, 0, 0, 0, "");
-
-                    if (isValid(addPartNameTxt.getText(), addPartPriceTxt.getText(), addPartInvTxt.getText(), addPartMinTxt.getText(),
-                            addPartMaxTxt.getText(), addPartDynamicTxt.getText())) {
-
-                        newOutsourced.setId(id);
-                        if (!addPartNameTxt.getText().isEmpty()) {
-                            newOutsourced.setName(addPartNameTxt.getText());
-                        }
-                        if (!addPartPriceTxt.getText().isEmpty()) {
-                            newOutsourced.setName(addPartPriceTxt.getText());
-                        }
-                        if (!addPartInvTxt.getText().isEmpty()) {
-                            newOutsourced.setName(addPartInvTxt.getText());
-                        }
-                        if (!addPartMinTxt.getText().isEmpty()) {
-                            newOutsourced.setName(addPartMinTxt.getText());
-                        }
-                        if (!addPartMaxTxt.getText().isEmpty()) {
-                            newOutsourced.setName(addPartMaxTxt.getText());
-                        }
-                        if (!addPartDynamicTxt.getText().isEmpty()) {
-                            newOutsourced.setName(addPartDynamicTxt.getText());
-                        }
-
-                        inventory.addPart(newOutsourced);
-                        System.out.println("Part Added");
-
-                        Stage stage;
-                        Parent root;
-                        stage = (Stage) addPartsaveBtn.getScene().getWindow();
-                        FXMLLoader loader = new FXMLLoader(getClass().getResource("/ViewController/MainScreen.fxml"));
-                        root = loader.load();
-                        Scene scene = new Scene(root);
-                        stage.setScene(scene);
-                        stage.show();
                 }
-
-                public boolean isValid(String partName, String partStock, String partPrice, String partMax, String partMin, String partOrigin) throws IOException {
-                    String errorMessage = "";
-                    Integer intMin = null, intMax = null;
-                    boolean isValid;
-
-                    if (partName == null || addPartNameTxt.isEmpty()){
-                        errorMessage += ("Need to input part name\n");
-                    }
-
-                }
-
-
-
-
-
-
-
-
-
-
-
-
-
-//                    int id = Integer.parseInt(addPartIdTxt.getText());
-                    String name = addPartNameTxt.getText();
-                    int inv = Integer.parseInt(addPartInvTxt.getText());
-                    int max = Integer.parseInt(addPartMaxTxt.getText());
-                    int min = Integer.parseInt(addPartMinTxt.getText());
-                    double price = Double.parseDouble(addPartPriceTxt.getText());
-
-
-                    if (addPartInhouseRBtn.isSelected()) {
-                        int machineId = Integer.parseInt(addPartMachineTxt.getText());
-                        InHouse newInhousePart = new InHouse(id, name, price, inv, min, max, machineId);
-                        Inventory.addPart(newInhousePart);
-                    }
-                    if (addPartOutsourcedRBtn.isSelected()) {
-                        String Cname = addPartCnameTxt.getText();
-                        Outsourced newOutsourcedPart = new Outsourced(id, name, price, inv, min, max, Cname);
-                        Inventory.addPart(newOutsourcedPart);
-
-                    }
-                  
-
-
-                }
-
-
-
-
 
             }
+        }
+
+
+
+        @FXML
+        public void onActionCancel(javafx.event.ActionEvent event) throws IOException {
+
+            stage = (Stage) ((Button) event.getSource()).getScene().getWindow();
+            scene = FXMLLoader.load(getClass().getResource("MainScreen.fxml"));
+            stage.setScene(new Scene((Parent) scene));
+            stage.show();
+        }
+
+
+
+
+    private boolean isValid(String partName, String partPrice, String partInv, String partMin, String partMax, String toggleGroup ) {
+
+            String errorMessage = "";
+            Integer intMin = null, intMax = null;
+            boolean isValid;
+
+
+
+        if (partName == null || partName.isEmpty()) {
+            errorMessage += ("Need to input part name\n");
+        }
+
+        try {
+            intMin = Integer.parseInt(partMin);
+        } catch (NumberFormatException e) {
+            errorMessage += ("Min must a number\n");
+        }
+
+        try {
+            intMax = Integer.parseInt(partMax);
+        } catch (NumberFormatException e) {
+            errorMessage += ("Maximum must be a number\n");
+        }
+
+        try {
+            if (intMin > intMax) {
+                errorMessage += ("Minimum must be less than maximum \n");
+            }
+            if (intMin < 0 || intMax < 0) {
+                errorMessage += ("Quantity cannot be less than zero\n");
+            }
+        } catch (NullPointerException e) {
+            errorMessage += ("Min and Max cannot be less than zero \n");
+        }
+        try {
+            int intInv = Integer.parseInt(partInv);
+
+            if (intMax != null && intMin != null) {
+                if (intInv < intMin || intInv > intMax) {
+                    errorMessage += ("Inventory must be between minimum and maximum \n");
+                }
+            } else {
+                errorMessage += ("Inventory cannot be blank \n");
+            }
+        } catch (NumberFormatException e) {
+            errorMessage += ("Inventory cannot be blank and must be a number\n");
+        }
+        try {
+            double price = Double.parseDouble(partPrice);
+            if (price < 0) {
+                errorMessage += ("Price cannot be less than zero\n");
+            }
+        } catch (NumberFormatException e) {
+            errorMessage += ("Price cannot be blank and must be a number\n");
+        }
+
+        if (!isOutsourced) {
+            if (!toggleGroup.isEmpty()) {
+                try {
+                    Integer.parseInt(toggleGroup);
+                } catch (NumberFormatException e) {
+                    errorMessage += ("MachineId must be a number");
+                }
+            } else {
+                errorMessage += ("MachineId cannot be black\n");
+            }
+        } else {
+            if (toggleGroup.isEmpty()) {
+                errorMessage += ("Company name cannot be blank \n");
+            }
+        }
+        if (errorMessage.isEmpty() == true) {
+            isValid = true;
+        } else {
+            isValid = false;
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Part Validation error");
+            alert.setHeaderText("Error");
+            alert.setContentText(errorMessage);
+            alert.showAndWait();
+        }
+
+        return isValid;
+    }
+
+}
+
+
+
+//    //                    int id = Integer.parseInt(addPartIdTxt.getText());
+//    String name = addPartNameTxt.getText();
+//    int inv = Integer.parseInt(addPartInvTxt.getText());
+//    int max = Integer.parseInt(addPartMaxTxt.getText());
+//    int min = Integer.parseInt(addPartMinTxt.getText());
+//    double price = Double.parseDouble(addPartPriceTxt.getText());
+//
+//
+//            if(addPartInhouseRBtn.isSelected()){
+//        int machineId = Integer.parseInt(addPartMachineTxt.getText());
+//        InHouse newInhousePart = new InHouse(id, name, price, inv, min, max, machineId);
+//        Inventory.addPart(newInhousePart);
+//    }
+//            if(addPartOutsourcedRBtn.isSelected())
+//
+//    {
+//        String Cname = addPartCnameTxt.getText();
+//        Outsourced newOutsourcedPart = new Outsourced(id, name, price, inv, min, max, Cname);
+//        Inventory.addPart(newOutsourcedPart);
+//
+//    }
+
+
+
+
+
+
+
+
 
 
