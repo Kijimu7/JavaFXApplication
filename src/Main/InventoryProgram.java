97087287388dace6b8ad85fb2a8605faeefb48b6
Kijimj7 <pinkokinawa84@gmail.com>
2020-06-30 18:17:20 -0700

    The table is working correctely, add part is done

--- src/Main/InventoryProgram.java
+++ src/Main/InventoryProgram.java
@@ -31,18 +31,19 @@ InventoryProgram extends Application {
         launch(args);
     }
 
-
     @Override
-    public void start(Stage stage) throws Exception {
+    public void start(Stage primaryStage) throws Exception {
 
         Inventory inventory = new Inventory();
         addTestData(inventory);
 
-        Parent root = FXMLLoader.load(getClass().getResource("../ViewController/MainScreen.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/ViewController/MainScreen.fxml"));
         Scene scene = new Scene(root);
-        stage.setScene(scene);
-        stage.show();
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
+
+
 
 
 
